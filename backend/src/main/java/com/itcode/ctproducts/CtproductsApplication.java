fd55dd88e540549804211bea6565beba4a2b041c
AndersonSAndrade <asantos919@gmail.com>
2020-12-09 14:56:01 -0300

    Subindo projeto Catalogo de Produtos com Back-End e Front-End

---
+++ backend/src/main/java/com/itcode/ctproducts/CtproductsApplication.java
@@ -0,0 +1,13 @@
+package com.itcode.ctproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtproductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtproductsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/itcode/ctproducts/CtproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcode.ctproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtproductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
