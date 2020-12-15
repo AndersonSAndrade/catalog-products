efa2fc7b637bfa593e95b965f61d064eaad9dd9f
AndersonSAndrade <asantos919@gmail.com>
2020-12-15 13:49:45 -0300

    Iniciando Front-End

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    <h1>Olã</h1>,
+    document.getElementById('root')
+);
