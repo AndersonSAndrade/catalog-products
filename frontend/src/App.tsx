f7f81b8396cbb9d965522d796a844926c078a51b
AndersonSAndrade <asantos919@gmail.com>
2020-12-15 15:22:09 -0300

    Adicionando o Navbar e os styles.scss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+import './core/assets/styles/custom.scss';
+import Navbar from './core/components/Navbar';
+
+const App = () => {
+    return (
+       <Navbar/>
+    )
+}
+
+export default App
